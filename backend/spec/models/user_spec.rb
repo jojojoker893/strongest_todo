3a3b870337100a98b2ee415786a381c7efd547dc
jojojoker893 <jojojoker114514@gmail.com>
2025-10-18 15:40:49 +0900

    test: UserModelSpecを記述

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -9,6 +9,49 @@ RSpec.describe User do
       end
     end
 
+    context "nameがnilの場合" do
+      let(:user) {build(:user, name: nil)}
+      it "保存されないこと" do
+        is_valid = user.valid?
+
+        expect(is_valid).to be false
+        expect(user.errors[:name]).to include("can't be blank")  
+      end
+    end
+    
+    context "emailがnilの場合" do
+      let(:user) {build(:user, email: nil)}
+      it "保存されないこと" do
+        is_valid = user.valid?
+
+        expect(is_valid).to be false
+        expect(user.errors[:email]).to include("can't be blank")   
+      end
+    end
+    
+    context "passwordがnilの場合" do
+      let(:user) {build(:user, password: nil)}
+
+      it "保存されないこと" do
+        is_valid = user.valid?
+
+        expect(is_valid).to be false
+        expect(user.errors[:password]).to include("can't be blank")    
+      end
+    end
+
+    context "emailが重複している場合" do
+      let!(:user) {create(:user, email: "test@example.com")}
+
+      it "保存されないこと" do
+        user = build(:user, email: "test@example.com")
+        user.valid?
+        expect(user.errors[:email]).to include("has already been taken")
+      end
+    end
+    
+    
+
 
   end
 end
