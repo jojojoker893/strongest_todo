ae738fb163a8256975bbb0390566644da88a721f
jojojoker893 <jojojoker114514@gmail.com>
2026-01-21 07:41:14 +0900

    task-rspecのPOST項目を記述

--- backend/spec/requests/api/v1/tasks_spec.rb
+++ backend/spec/requests/api/v1/tasks_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Tasks', type: :request do
-  let!(:user) { FactoryBot.create(:user) }
-  let!(:other_user) { FactoryBot.create(:user, :other_user) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:other_user) { FactoryBot.create(:user, :other_user) }
+  let(:token) { JwtToken.call(user) }
+  let(:headers) {{ "Authorization" => "Bearer #{token}" }}
   describe  "GET /api/v1/tasks" do
     let!(:task) { FactoryBot.create(:task, user: user) }
     let!(:other_task) { FactoryBot.create(:task, :other_task, user: other_user) }
 
     context "認証している場合" do
-      let(:token) { JwtToken.call(user) }
-      let(:headers) {{ "Authorization" => "Bearer #{token}" }}
       it "自身のタスクのみ取得できること" do
         get "/api/v1/tasks", headers: headers
         expect(response).to have_http_status(:ok)
@@ -26,8 +26,6 @@ RSpec.describe 'Api::V1::Tasks', type: :request do
   describe "POST /api/v1/tasks" do
     context "認証しており、正しいパラメータの場合" do
       let!(:valid_params) { { task: FactoryBot.attributes_for(:task)} }
-      let(:token) { JwtToken.call(user) }
-      let(:headers) {{ "Authorization" => "Bearer #{token}" }}
       it "タスクを作成できること" do
         expect{
           post "/api/v1/tasks", headers: headers, params: valid_params
@@ -39,26 +37,34 @@ RSpec.describe 'Api::V1::Tasks', type: :request do
 
     context "認証しており、不正なパラメータの場合" do
       let!(:invalid_params) { { task: FactoryBot.attributes_for(:task, :invalid_task) } }
-      let(:token) { JwtToken.call(user) }
-      let(:headers) {{ "Authorization" => "Bearer #{token}" }}
       it "タスクを作成できないこと" do
         expect{
           post "/api/v1/tasks", headers: headers, params: invalid_params
         }.not_to change(Task, :count)
 
-        expect(response).to have_http_status(:unprocessable_entity)  
+        expect(response).to have_http_status(:unprocessable_content)  
       end
     end
 
     context "認証しておらず、正しいパラメータの場合" do
+        let!(:valid_params) { { task: FactoryBot.attributes_for(:task)} }
       it "タスクを作成できないこと" do
-        
+        expect{
+          post "/api/v1/tasks", params: valid_params
+      }.not_to change(Task, :count)
+
+      expect(response).to have_http_status(:unauthorized)  
       end
     end
 
     context "認証しておらず、不正なパラメータの場合" do
+      let!(:invalid_params) { { task: FactoryBot.attributes_for(:task, :invalid_task) } }
       it "タスクを作成できないこと" do
-        
+        expect{
+          post "/api/v1/tasks", params: invalid_params
+      }.not_to change(Task, :count)
+
+      expect(response).to have_http_status(:unauthorized)  
       end
     end
     
